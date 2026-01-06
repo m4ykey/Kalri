e8401877cdb40f5a851e92b11373940549526a88
Michał <michalfr1601@gmail.com>
2026-01-06 17:48:03 +0100

    feat: Hardware detection

--- app/src/main/java/com/m4ykey/kalri/MainActivity.kt
+++ app/src/main/java/com/m4ykey/kalri/MainActivity.kt
@@ -1,34 +1,63 @@
 package com.m4ykey.kalri
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.AudioDeviceCallback
+import android.media.AudioDeviceInfo
+import android.media.AudioManager
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.m4ykey.kalri.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var audioManager : AudioManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
 
-        // Example of a call to a native method
+        checkAudioDevice()
+
+        audioManager.registerAudioDeviceCallback(object : AudioDeviceCallback() {
+            override fun onAudioDevicesAdded(addedDevices: Array<out AudioDeviceInfo?>?) {
+                checkAudioDevice()
+            }
+
+            override fun onAudioDevicesRemoved(removedDevices: Array<out AudioDeviceInfo?>?) {
+                showToast(this@MainActivity, "Disconnected")
+            }
+        }, null)
+        
         binding.sampleText.text = stringFromJNI()
     }
-
-    /**
-     * A native method that is implemented by the 'kalri' native library,
-     * which is packaged with this application.
-     */
+    
     external fun stringFromJNI(): String
 
     companion object {
-        // Used to load the 'kalri' library on application startup.
         init {
             System.loadLibrary("kalri")
+        }
+    }
+    
+    private fun checkAudioDevice() {
+        val devices = audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS)
+        for (device in devices) {
+            when (device.type) {
+                AudioDeviceInfo.TYPE_USB_DEVICE,
+                AudioDeviceInfo.TYPE_USB_HEADSET,
+                AudioDeviceInfo.TYPE_USB_ACCESSORY -> {
+                    showToast(this, "Detected: ${device.productName}")
+                }
+
+                AudioDeviceInfo.TYPE_WIRED_HEADPHONES,
+                AudioDeviceInfo.TYPE_WIRED_HEADSET -> {
+                    showToast(this, "Detected Wired Headphones")
+                }
+            }
         }
     }
 }

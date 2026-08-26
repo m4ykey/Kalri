07041afe462ad2ce9f6377bac6319531d0d4f762
Michał <michalfr1601@gmail.com>
2026-08-27 00:23:32 +0200

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.m4ykey.kalri"
         minSdk = 29
         targetSdk = 37
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 102
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
